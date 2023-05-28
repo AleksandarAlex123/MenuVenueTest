9bc9bcc4a8898e89f7bf7baa04262d9cc14d81db
AleksandarAdam <aleksandar.adamovic@ingsoftware.com>
2023-05-28 22:34:00 +0200

    Added Firebase

--- app/src/main/java/com/aleksandar/menutest/presentation/MenuApp.java
+++ app/src/main/java/com/aleksandar/menutest/presentation/MenuApp.java
@@ -2,7 +2,15 @@ package com.aleksandar.menutest.presentation;
 
 import android.app.Application;
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+
 import dagger.hilt.android.HiltAndroidApp;
 
 @HiltAndroidApp
-public class MenuApp extends Application {}
+public class MenuApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseCrashlytics.getInstance().setCrashlyticsCollectionEnabled(true);
+    }
+}
